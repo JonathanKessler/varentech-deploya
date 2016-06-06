69ff49477276ffa573bdf1d0188150b1ad8e4ca7
kesslerk <kesslerk@varentech.com>
2016-06-06 14:45:39 -0400

    Add files via upload

--- src/main/java/com/varentech/deploya/Form/ExecuteJar.java
+++ src/main/java/com/varentech/deploya/Form/ExecuteJar.java
@@ -4,6 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import java.io.FileInputStream;
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+
+
 
 public class ExecuteJar {
 
@@ -14,8 +20,8 @@ public class ExecuteJar {
             // print a message
             System.out.println("Executing HelloWorldJar.jar");
 
-            // create a process and execute notepad.exe
-            //Process process = Runtime.getRuntime().exec("notepad.exe");
+
+            //execute first jar
             Process p = Runtime.getRuntime().exec("java -jar C:\\Users\\kesslerk\\Documents\\HelloWorldJar\\out\\artifacts\\HelloWorldJar_jar\\HelloWorldJar.jar");
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(p.getInputStream()));
@@ -23,8 +29,38 @@ public class ExecuteJar {
             while ((line = in.readLine()) != null) {
                 System.out.println(line);
             }
+
+            //execute second jar
+            p = Runtime.getRuntime().exec("java -jar C:\\Users\\kesslerk\\Documents\\AnotherJar\\out\\artifacts\\AnotherJar_jar\\AnotherJar.jar");
+            in = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()));
+            line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            //print hash of tar
+            Hash hash = new Hash();
+            System.out.println(hash.checkSum("C:\\Users\\kesslerk\\Documents\\test.tar"));
+
+
+            //print all file names in tar
+            TarArchiveInputStream tarInput = new TarArchiveInputStream(new FileInputStream("C:\\Users\\kesslerk\\Documents\\test.tar"));
+
+            TarArchiveEntry entry;
+            while (null!=(entry=tarInput.getNextTarEntry())) {
+                System.out.println(entry.getName());
+                //System.out.println(entry.hashCode());
+
+            }
+
+
+
             // print another message
             System.out.println("It should now open.");
+
+
+
 
 
         } catch (IOException e) {
