1d3f6ea98d3b30e25564f72b83cf88334b58f6d9
kesslerk <kesslerk@varentech.com>
2016-06-06 15:13:29 -0400

    Add files via upload

--- src/main/java/com/varentech/deploya/doaimpl/EntriesDetailsDoaImpl.java
+++ src/main/java/com/varentech/deploya/doaimpl/EntriesDetailsDoaImpl.java
@@ -1,6 +1,8 @@
 package com.varentech.deploya.doaimpl;
 
+import com.varentech.deploya.Form.Resource;
 import com.varentech.deploya.doa.DatabaseInterface;
+import com.varentech.deploya.entities.Entries;
 import com.varentech.deploya.entities.EntriesDetail;
 import com.varentech.deploya.util.ConnectionConfiguration;
 
@@ -16,7 +18,10 @@ public class EntriesDetailsDoaImpl implements DatabaseInterface {
   }
 */
 
-  public void insert(EntriesDetail entriesDetail) {
+  public void insertIntoEntries() {
+
+    Resource res = new Resource();
+
     try {
       Connection connection = ConnectionConfiguration.getConnection();
       PreparedStatement preparedStatement = connection.prepareStatement(
@@ -24,23 +29,30 @@ public class EntriesDetailsDoaImpl implements DatabaseInterface {
                       "(time_stamp, username, file_name, path_to_local_file, path_to_destination, unpack_args, execute_args) " +
                       "VALUES (?, ?, ?, ?, ?, ?, ?)"
       );
-      preparedStatement.setString(1, entriesDetail.getTime().toString());
-      preparedStatement.setString(2, entriesDetail.getUserName());
-      preparedStatement.setString(3, entriesDetail.getFileName());
-      preparedStatement.setString(4, entriesDetail.getPathToLocalFile());
-      preparedStatement.setString(5, entriesDetail.getPathToDestination());
-      preparedStatement.setString(6, entriesDetail.getUnpackArguments());
-      preparedStatement.setString(7, entriesDetail.getExecuteArguments());
+      preparedStatement.setString(1, res.entry.getTime().toString());
+      preparedStatement.setString(2, res.entry.getUserName());
+      preparedStatement.setString(3, res.entry.getFileName());
+      preparedStatement.setString(4, res.entry.getPathToLocalFile());
+      preparedStatement.setString(5, res.entry.getPathToDestination());
+      preparedStatement.setString(6, res.entry.getUnpackArguments());
+      preparedStatement.setString(7, res.entry.getExecuteArguments());
       preparedStatement.executeUpdate();
     } catch (SQLException se) {
       se.printStackTrace();
     }
+
+  }
+
+  public void insertIntoEntriesDetail() {
+
+    Resource res = new Resource();
+
     //Select the Entries id and storing that into a local variable using the time stamp.
     try {
       Connection connection = ConnectionConfiguration.getConnection();
       Statement statement = connection.createStatement();
       ResultSet resultSet = statement.executeQuery(
-              "SELECT id FROM Entries WHERE time_stamp = " + "'" + entriesDetail.getTime() + "'"
+              "SELECT id FROM Entries WHERE time_stamp = " + "'" + res.entry.getTime() + "'"
       );
       int id = resultSet.getInt("id");
 
@@ -51,9 +63,9 @@ public class EntriesDetailsDoaImpl implements DatabaseInterface {
       );
 
       preparedStatement.setString(1, String.valueOf(id));
-      preparedStatement.setString(2, entriesDetail.getFileName());
-      preparedStatement.setString(3, String.valueOf(entriesDetail.getHashValue()));
-      preparedStatement.setString(4, String.valueOf(entriesDetail.getOutput()));
+      preparedStatement.setString(2, res.entriesDetail.getFileName());
+      preparedStatement.setString(3, String.valueOf(res.entriesDetail.getHashValue()));
+      preparedStatement.setString(4, String.valueOf(res.entriesDetail.getOutput()));
       preparedStatement.executeUpdate();
 
     } catch (SQLException e) {
