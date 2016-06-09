fbfc09d33e43f00384ad84fb510182ff0361a29c
kesslerk <kesslerk@varentech.com>
2016-06-09 13:51:10 -0400

    Add files via upload

---
+++ src/main/java/com/varentech/deploya/Form/Hash.java
@@ -0,0 +1,52 @@
+package com.varentech.deploya.Form;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.*;
+
+//http://javarevisited.blogspot.com/2013/06/how-to-generate-md5-checksum-for-files.html
+public class Hash {
+    private static final Logger logger = Logger.getLogger(Hash.class.getName());
+
+    public static String checkSum(File file) {
+        //String file = "C:\\Users\\kesslerk\\Documents\\HelloWorldJar\\out\\artifacts\\HelloWorldJar_jar\\HelloWorldJar.jar"; //location of the file
+
+        String hash_value = null;
+        try {
+            hash_value = DigestUtils.md5Hex(new FileInputStream(file));
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+        return hash_value;
+    }
+
+
+    public String createSha1(File file)  {
+        //File file = new File(file_name);
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-1");
+            InputStream fis = new FileInputStream(file);
+            int n = 0;
+            byte[] buffer = new byte[8192];
+            while (n != -1) {
+                n = fis.read(buffer);
+                if (n > 0) {
+                    digest.update(buffer, 0, n);
+                }
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return digest.digest().toString();
+    }
+}
+
---
+++ src/main/java/com/varentech/deploya/Form/Unpack.java
@@ -0,0 +1,55 @@
+package com.varentech.deploya.Form;
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.utils.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Unpack {
+
+    public void unpack(File archive) throws IOException{
+        Resource res = new Resource();
+
+        //if we are going to unpack
+        if(res.entry.getUnpackArguments()!=null){
+            //unpack file here
+
+            //Read TAR file into TarArchiveInputStream
+            TarArchiveInputStream myTarFile = new TarArchiveInputStream(new FileInputStream(archive));
+
+            //To read each individual TAR file.
+            TarArchiveEntry entry = null;
+            String individualFiles;
+            int offset;
+            FileOutputStream outputFile = null;
+            //Create a loop to read every single entry in TAR file.
+            while((entry = myTarFile.getNextTarEntry()) != null){
+                //Get the name of the file.
+                individualFiles = entry.getName();
+                //Get the size of the file and create a byte array for the size.
+                byte[] content = new byte[(int) entry.getSize()];
+                offset = 0;
+                //Some SOP statements to check progress.
+                System.out.println("File Name in TAR File is: " + individualFiles);
+                System.out.println("Sive of the file is: " + entry.getSize());
+                System.out.println("Byte Array length: " + content.length);
+
+                //Read file from the archive into a byte array
+                myTarFile.read(content,offset,content.length - offset);
+                //Define OutputStream for writing the file.
+                outputFile = new FileOutputStream(new File(individualFiles));
+                //Use IOUtils to write content of byte array to physical file.
+                IOUtils.copy(myTarFile, outputFile);
+
+                //Close Output Stream
+                outputFile.close();
+            }
+            //close TarArchiveInputStream
+            myTarFile.close();
+        }
+    }
+}
