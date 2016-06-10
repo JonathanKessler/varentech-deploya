f4387db3c3b4b67f7e45d7d2194ac6c338e5fa6e
kesslerk <kesslerk@varentech.com>
2016-06-10 15:24:37 -0400

    Add files via upload

--- src/main/java/com/varentech/deploya/Form/GetConfigProps.java
+++ src/main/java/com/varentech/deploya/Form/GetConfigProps.java
@@ -3,7 +3,6 @@ package com.varentech.deploya.Form;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
 import java.util.Enumeration;
 import java.util.Properties;
 
@@ -24,18 +23,6 @@ public class GetConfigProps {
                 throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
             }
 
-            Date time = new Date(System.currentTimeMillis());
-
-            // get the property value and print it out
-            String port_number = prop.getProperty("port_number");
-            String default_directory = prop.getProperty("default_directory");
-            String log_level=prop.getProperty("log_level");
-            String acceptable_extensions=prop.getProperty("acceptable_extensions");
-
-            result = "Configurations: " + port_number + " ," + default_directory + " ," + log_level + " ," +acceptable_extensions ;
-            System.out.println(result + "\nProgram Ran on " + time );
-
-            //if you have a lot of key-value pairs this way is much simpler
             Enumeration enuKeys = prop.keys();
             while (enuKeys.hasMoreElements()) {
                 String key = (String) enuKeys.nextElement();
