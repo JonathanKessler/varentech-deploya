364f484ef4d72c2d955f18a093e4620a3294fcd7
Maria Raynor <mraynor95@yahoo.com>
2016-06-13 10:14:39 -0400

    Update LoginServlet.java

--- src/main/java/com/varentech/deploya/Form/LoginServlet.java
+++ src/main/java/com/varentech/deploya/Form/LoginServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
