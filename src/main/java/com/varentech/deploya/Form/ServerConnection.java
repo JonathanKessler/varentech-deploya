175731e5db053b2d60d328d2d5afa666cdc3b48d
Maria Raynor <mraynor95@yahoo.com>
2016-06-13 10:13:34 -0400

    Update ServerConnection.java

--- src/main/java/com/varentech/deploya/Form/ServerConnection.java
+++ src/main/java/com/varentech/deploya/Form/ServerConnection.java
@@ -20,7 +20,8 @@ public class ServerConnection {
     /**
      * This method connects servlets together.
      */
-
+     
+  private Logger logger= LoggerFactory.getLogger(ServerConnection.class);
     public void connect() {
         ResourceConfig config = new ResourceConfig();
         config.packages("com/varentech/deploya");
@@ -41,6 +42,7 @@ public class ServerConnection {
         //connect to server
         try {
             server.start();
+            logger.info("Successfully connected to server.");
             server.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
