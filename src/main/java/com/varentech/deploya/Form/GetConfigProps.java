e886e690c9fc669ba1800144fdaddf35d28a3a92
kesslerk <kesslerk@varentech.com>
2016-06-13 12:28:13 -0400

    Update GetConfigProps.java

--- src/main/java/com/varentech/deploya/Form/GetConfigProps.java
+++ src/main/java/com/varentech/deploya/Form/GetConfigProps.java
@@ -1,32 +1,24 @@
 package com.varentech.deploya.Form;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 /**
  *
  * This class gets properties from the configuration file.
  * @author VarenTech
- * @see java.io.FileNotFoundException
  * @see java.io.IOException
- * @see java.io.InputStream
- * @see java.util.Enumeration
  * @see java.util.Properties
  *
  */
 
 public class GetConfigProps {
-    String result = " ";
-    InputStream inputStream;
+   
     private static final Properties prop = new Properties();
     /**
-     *
-     * @return String that has the the value of result????
+     * @loads config file
      * @throws IOException
      */
-
-    //public void getPropValues() throws IOException {
         static {
             try {
                 ClassLoader loader = Thread.currentThread().getContextClassLoader();
@@ -37,36 +29,11 @@ public class GetConfigProps {
         }
 
 
+        /**
+         * @returns String property of corresponding key value
+         */
         public static String getSetting(String key) {
             return prop.getProperty(key);
         }
-
-
-
-        /*
-        try {
-
-            String propFileName = "config.properties";
-            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-
-            if (inputStream != null) {
-                prop.load(inputStream);
-            } else {
-                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-            }
-            System.out.println(prop.getProperty("port_number") );
-            Enumeration enuKeys = prop.keys();
-            while (enuKeys.hasMoreElements()) {
-                String key = (String) enuKeys.nextElement();
-                String value = prop.getProperty(key);
-            }
-        } catch (Exception e) {
-            System.out.println("Exception: " + e);
-        } finally {
-            inputStream.close();
-        }
-*/
-       // return result;
-
 
 }
