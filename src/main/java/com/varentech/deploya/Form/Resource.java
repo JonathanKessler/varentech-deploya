347659a7d713aebfffcdde560ebe1dee65cec23b
kesslerk <kesslerk@varentech.com>
2016-06-13 09:18:00 -0400

    Update Resource.java

--- src/main/java/com/varentech/deploya/Form/Resource.java
+++ src/main/java/com/varentech/deploya/Form/Resource.java
@@ -1,8 +1,5 @@
 package com.varentech.deploya.Form;
 
-import com.varentech.deploya.Driver;
-import com.varentech.deploya.directories.LocalDirectories;
-import com.varentech.deploya.doaimpl.EntriesDetailsDoaImpl;
 import com.varentech.deploya.entities.Entries;
 import com.varentech.deploya.entities.EntriesDetail;
 
@@ -33,14 +30,4 @@ public class Resource {
     public static Entries entry = new Entries();
     public static EntriesDetail entriesDetail = new EntriesDetail();
 
-   /* @POST
-    @Path("post")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.TEXT_HTML)
-    public String postMethod(@FormParam("file_name") File file,
-                             @FormParam("path_to_destination") String path_to_destination,
-                             @FormParam("execute_args") String execute_args,
-                             @FormParam("unpack_args") String unpack_args) {
-
-  */
 }
