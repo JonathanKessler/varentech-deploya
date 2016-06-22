b3981a8f9979f5542f49fe5cc0917cae56610b2c
kesslerk <kesslerk@varentech.com>
2016-06-22 11:25:36 -0400

    Update ProcessFile.java

--- src/main/java/com/varentech/deploya/Form/ProcessFile.java
+++ src/main/java/com/varentech/deploya/Form/ProcessFile.java
@@ -5,8 +5,6 @@ import com.google.common.hash.Hashing;
 import com.google.common.io.Files;
 import com.varentech.deploya.doaimpl.EntriesDetailsDoaImpl;
 import com.varentech.deploya.entities.EntriesDetail;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,8 +12,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.*;
 
 
