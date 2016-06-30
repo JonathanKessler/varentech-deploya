d01fb25ef51a0e48583fc568d36832d4e58e9225
kesslerk <kesslerk@varentech.com>
2016-06-30 10:24:22 -0400

    Update DatabaseInterface.java

--- src/main/java/com/varentech/deploya/dao/DatabaseInterface.java
+++ src/main/java/com/varentech/deploya/dao/DatabaseInterface.java
@@ -1,33 +1,18 @@
 package com.varentech.deploya.doa;
 
-import java.util.List;
 import com.varentech.deploya.entities.EntriesDetail;
 
-/*
-This interface consists of a few methods that could be used for a database
-implementation.
+/**
+ *Contains methods to add to the database
  */
-
 public interface DatabaseInterface {
-  /*
-  This method is used to create a new table in the
-  database if needed.
-   */
-  //void createTable();
-  /*
-  <p>This method is used to insert into the database
-  with the parameter EntriesDetail type object.
-  @param EntriesDetail  the object that needs to be inserted into the database.
-   */
-  void insertIntoEntries();
-  void insertIntoEntriesDetail(EntriesDetail entriesDetail);
-  /*
-  <p>This method is used to list down
-  all the record from the Entries table or the
-  Entries_Details table.
-  @return a list of the entries found in the database.
-   */
-  List<EntriesDetail> listEntries();
+    /**
+     *Insert into the Entries table of Deploya.db
+     */
+    void insertIntoEntries();
 
-
+    /**
+     *Insert into the EntriesDetails table of Deploya.db
+     */
+    void insertIntoEntriesDetail(EntriesDetail entriesDetail);
 }
