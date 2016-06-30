181faa81edc078a4310e1e0611567d5217cfe530
kesslerk <kesslerk@varentech.com>
2016-06-30 10:17:10 -0400

    Update saveTempDirectory.java

--- src/main/java/com/varentech/deploya/Form/saveTempDirectory.java
+++ src/main/java/com/varentech/deploya/Form/saveTempDirectory.java
@@ -1,4 +1,4 @@
-package com.varentech.deploya.Form;
+package com.varentech.deploya.form;
 
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.io.FileUtils;
@@ -11,11 +11,18 @@ import java.io.*;
 // reason. Just wondering.
 // TODO: Tav: Potentially can be broken down into smaller methods to improve
 // readability, but not necessary.
-public class saveTempDirectory {
 
-    private Logger logg = LoggerFactory.getLogger(saveTempDirectory.class);
+/**
+ *This class has methods to save all files into a temporary directory and the move those files out to the temporary directory.
+ */
+public class SaveTempDirectory {
 
-    // TODO: Tav: header comment. The name of this method isn't very descriptive
+    private Logger logg = LoggerFactory.getLogger(SaveTempDirectory.class);
+    
+    /**
+     *This method creates and saves all files to a temporary directory
+     * Calls methods to unpack files, move files to destination directory, execute files, and find the hash of all files.
+     */
     void directory(String file_name){
         Resource res = new Resource();
 
@@ -43,24 +50,8 @@ public class saveTempDirectory {
             File[] tempDirList = tempDir.listFiles();
 
             //moves unpacked files to destination if user checked directory
-            if(res.entry.getUnpackArguments()!=null){
-                for(File file: tempDirList){
-
-                    if(file != current){
-
-                        if(file.isDirectory() ==false) {
-                            inputStream = new FileInputStream(file);
-                            outputStream = new FileOutputStream(res.entry.getPathToDestination() + File.separator + file.getName());
-                            IOUtils.copy(inputStream, outputStream);
-                            outputStream.close();
-                        }else {
-                            File srcDir = new File(res.entry.getPathToDestination() + File.separator + file.getName());
-                            FileUtils.copyDirectory(file, srcDir);
-                        }
-                    }
-                }
-                logg.debug("{} has been unpacked into the destination.", file_name);
-            }
+            moveFiles(tempDirList, current);
+            logg.info("{} has been unpacked into the destination.", file_name);
 
             //execute jar/tar file and save output. Only execute if there was no error while unpacking
             if(res.entriesDetail.getError()=="" || (res.entriesDetail.getError()!="" && res.entry.getUnpackArguments()==null)) {
@@ -75,6 +66,41 @@ public class saveTempDirectory {
             logg.error("Exception while finding file: ", e);
         } catch (IOException e) {
             logg.error("Exception while saving or moving file to temporary directory: ", e);
+        }
+    }
+    
+    /**
+     *This method moves unpacked files into the destination directory if the user checked the box for unpacking.
+     */
+    public void moveFiles(File[] tempDirList, File current) {
+
+        Resource res = new Resource();
+
+        if (res.entry.getUnpackArguments() != null) {
+            for (File file : tempDirList) {
+
+                if (file != current) {
+
+                    try {
+                        if (file.isDirectory() == false) {
+                            InputStream inputStream = null;
+
+                            inputStream = new FileInputStream(file);
+
+                            OutputStream outputStream = new FileOutputStream(res.entry.getPathToDestination() + File.separator + file.getName());
+                            IOUtils.copy(inputStream, outputStream);
+                            outputStream.close();
+                        } else {
+                            File srcDir = new File(res.entry.getPathToDestination() + File.separator + file.getName());
+                            FileUtils.copyDirectory(file, srcDir);
+                        }
+                    } catch (FileNotFoundException e) {
+                        logg.error("Exception while finding file: ", e);
+                    } catch (IOException e) {
+                        logg.error("Exception while saving or moving file to temporary directory: ", e);
+                    }
+                }
+            }
         }
     }
 
