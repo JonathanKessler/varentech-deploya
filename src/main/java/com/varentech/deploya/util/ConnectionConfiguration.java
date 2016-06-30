89a512af84c055d58ab7ac519ed2c690c2347668
kesslerk <kesslerk@varentech.com>
2016-06-30 10:38:21 -0400

    Update ConnectionConfiguration.java

--- src/main/java/com/varentech/deploya/util/ConnectionConfiguration.java
+++ src/main/java/com/varentech/deploya/util/ConnectionConfiguration.java
@@ -2,21 +2,15 @@ package com.varentech.deploya.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * This class manages a connection to the database.
- * @author VarenTech
- * @see java.sql.Connection
- * @see java.sql.DriverManager
+ * This class connects to the database.
  */
 public class ConnectionConfiguration {
 
-  /**
-   * This method establishes a connection to a database, that can be configurable.
-   * @return Connection to the database, or an error if there was a problem connecting to it.
-     */
+   /**
+   * This method establishes a connection to a database.
+   */
   private static Logger logg = LoggerFactory.getLogger(ConnectionConfiguration.class);
   public static Connection getConnection(){
     Connection connection = null;
