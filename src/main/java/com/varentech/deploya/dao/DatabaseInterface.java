432543c64bff0a34475213a95803cc12927ac178
Maria Raynor <mraynor95@yahoo.com>
2016-07-05 15:29:19 -0400

    Update DatabaseInterface.java

--- src/main/java/com/varentech/deploya/dao/DatabaseInterface.java
+++ src/main/java/com/varentech/deploya/dao/DatabaseInterface.java
@@ -3,16 +3,16 @@ package com.varentech.deploya.dao;
 import com.varentech.deploya.entities.EntriesDetail;
 
 /**
- *Contains methods to add to the database
+ *Contains methods to add to the database.
  */
 public interface DatabaseInterface {
     /**
-     *Insert into the Entries table of Deploya.db
+     *Insert into the Entries table of Deploya.db.
      */
     void insertIntoEntries();
 
     /**
-     *Insert into the EntriesDetails table of Deploya.db
+     *Insert into the EntriesDetails table of Deploya.db.
      */
     void insertIntoEntriesDetail(EntriesDetail entriesDetail);
 }
