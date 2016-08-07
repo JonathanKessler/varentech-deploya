2628ce52ca71739c9fd2f7898d29b1d60f7ed488
Maria Raynor <mraynor95@yahoo.com>
2016-08-07 11:54:20 -0400

    Add files via upload

--- src/main/java/com/varentech/deploya/form/LoginServlet.java
+++ src/main/java/com/varentech/deploya/form/LoginServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ResourceBundle;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,9 +27,12 @@ public class LoginServlet extends HttpServlet {
         response.setContentType("text/html");
         logg.debug("Successfully connected to login servlet.");
         response.setStatus(HttpServletResponse.SC_OK);
-        ResourceBundle resource = ResourceBundle.getBundle("config");
-        String port = resource.getString("port_number");
-        String context_path = resource.getString("context_path");
+        Config config = ConfigFactory.load();
+        String port = config.getString("varentech.project.port_number");
+        String context_path = config.getString("varentech.project.context_path");
+        //ResourceBundle resource = ResourceBundle.getBundle("config");
+        //String port = resource.getString("port_number");
+        //String context_path = resource.getString("context_path");
 
         //set the username as a session attribute
         String username = request.getParameter("username");
