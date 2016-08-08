84091aba819d1641cd8cc723a7c4d517cdb31139
Maria Raynor <mraynor95@yahoo.com>
2016-08-08 14:31:02 -0400

    logg.info to logg.debug

--- src/main/java/com/varentech/deploya/util/DatabaseConnectivity.java
+++ src/main/java/com/varentech/deploya/util/DatabaseConnectivity.java
@@ -24,7 +24,7 @@ public class DatabaseConnectivity {
         //Load our own config values from the default location
         //application.conf
         Config config = ConfigFactory.load();
-        logg.info("Database name: " + config.getString("varentech.project.path_to_database"));
+        logg.debug("Database name: " + config.getString("varentech.project.path_to_database"));
         String pathOfDataBase = config.getString("varentech.project.path_to_database");
         ConnectionConfiguration.setPathToDataBase(pathOfDataBase);
     }
@@ -35,7 +35,7 @@ public class DatabaseConnectivity {
     public static void findDataBase(String path){
         //Load the given config values given from the arguments
         Config config = ConfigFactory.load(path);
-        logg.info("Loading values from the config file located at " + path);
+        logg.debug("Loading values from the config file located at " + path);
         String pathOfDataBase = config.getString("varentech.project.path_to_database");
         try{
             logg.info("Loading database path given at " + pathOfDataBase);
@@ -46,7 +46,7 @@ public class DatabaseConnectivity {
         }
         catch(Exception e){
             logg.error("File not found at " + pathOfDataBase);
-            logg.info("Using default values to create a database.");
+            logg.debug("Using default values to create a database.");
             findDatabase();
         }
     }
@@ -56,33 +56,33 @@ public class DatabaseConnectivity {
         //check to see if Entries table exists.
         ResultSet resultSet = metaData.getTables(null, null, "Entries", null);
         if (resultSet.next()){
-            logg.info("Entries table exists");
+            logg.debug("Entries table exists");
             //check to see if all of columns exists in Entries table.
             String tempColName;
             resultSet = metaData.getColumns(null, null, "Entries", "time_stamp");
             tempColName = "time_stamp";
-            if(resultSet.next()){logg.info("time_stamp column exists");}
+            if(resultSet.next()){logg.debug("time_stamp column exists");}
             resultSet = metaData.getColumns(null, null, "Entries", "username");
             tempColName = "username";
-            if(resultSet.next()){logg.info("username column exists");}
+            if(resultSet.next()){logg.debug("username column exists");}
             resultSet = metaData.getColumns(null, null, "Entries", "file_name");
             tempColName = "file_name";
-            if(resultSet.next()){logg.info("file_name column exists");}
+            if(resultSet.next()){logg.debug("file_name column exists");}
             resultSet = metaData.getColumns(null, null, "Entries", "path_to_local_file");
             tempColName = "path_to_local_file";
-            if(resultSet.next()){logg.info("path_to_local_file column exists");}
+            if(resultSet.next()){logg.debug("path_to_local_file column exists");}
             resultSet = metaData.getColumns(null, null, "Entries", "path_to_destination");
             tempColName = "path_to_destination";
-            if(resultSet.next()){logg.info("path_to_destination column exists");}
+            if(resultSet.next()){logg.debug("path_to_destination column exists");}
             resultSet = metaData.getColumns(null, null, "Entries", "unpack_args");
             tempColName = "unpack_args";
-            if(resultSet.next()){logg.info("unpack_args column exists");}
+            if(resultSet.next()){logg.debug("unpack_args column exists");}
             resultSet = metaData.getColumns(null, null, "Entries", "execute_args");
             tempColName = "execute_args";
-            if(resultSet.next()){logg.info("execute_args column exists");}
+            if(resultSet.next()){logg.debug("execute_args column exists");}
             resultSet = metaData.getColumns(null, null, "Entries", "archive");
             tempColName = "archive";
-            if(resultSet.next()){logg.info("archive column exists");}
+            if(resultSet.next()){logg.debug("archive column exists");}
             else{
                 logg.error("Necessary column, " + tempColName + " does not exist in Entries table.");
                 logg.error("Program will exit with Error code 1.");
@@ -90,31 +90,31 @@ public class DatabaseConnectivity {
             }
         }
         else{
-            logg.info("Entries table does not exist, creating Entries table...");
+            logg.debug("Entries table does not exist, creating Entries table...");
             EntriesDetailsDaoImpl impl = new EntriesDetailsDaoImpl();
             impl.createEntriesTable();
         }
         //check for Entries_Details table
         resultSet = metaData.getTables(null, null, "Entries_Details", null);
         if(resultSet.next()){
-            logg.info("Entries_Details table exists");
+            logg.debug("Entries_Details table exists");
             //Need to check that each column exists in Entries_Details table
             String tempColName;
             resultSet = metaData.getColumns(null, null, "Entries_Details", "entries_id");
             tempColName = "entries_id";
-            if(resultSet.next()){logg.info("entries_id column exists");}
+            if(resultSet.next()){logg.debug("entries_id column exists");}
             resultSet = metaData.getColumns(null, null, "Entries_Details", "file_name");
             tempColName = "file_name";
-            if(resultSet.next()){logg.info("file_name column exists");}
+            if(resultSet.next()){logg.debug("file_name column exists");}
             resultSet = metaData.getColumns(null, null, "Entries_Details", "hash_value");
             tempColName = "hash_value";
-            if(resultSet.next()){logg.info("hash_value column exists");}
+            if(resultSet.next()){logg.debug("hash_value column exists");}
             resultSet = metaData.getColumns(null, null, "Entries_Details", "output");
             tempColName = "output";
-            if(resultSet.next()){logg.info("output column column exists");}
+            if(resultSet.next()){logg.debug("output column column exists");}
             resultSet = metaData.getColumns(null, null, "Entries_Details", "error");
             tempColName = "error";
-            if (resultSet.next()){logg.info("error column exists");}
+            if (resultSet.next()){logg.debug("error column exists");}
             else{
                 logg.error("Necessary column, " + tempColName + " does not exist in Entries_Details table.");
                 logg.error("Program will exit with Error code 1.");
@@ -122,7 +122,7 @@ public class DatabaseConnectivity {
             }
         }
         else{
-            logg.info("Entries_Details table does not exits, creating Entries_Details table...");
+            logg.debug("Entries_Details table does not exits, creating Entries_Details table...");
             EntriesDetailsDaoImpl impl = new EntriesDetailsDaoImpl();
             impl.createEntriesDetailsTable();
         }
