025472d2e2065acc11e947503e9aec24d7b7d71e
kesslerk <kesslerk@varentech.com>
2016-08-10 10:14:22 -0400

    Add files via upload

--- src/test/java/EntriesDetailsDaoImplTest.java
+++ src/test/java/EntriesDetailsDaoImplTest.java
@@ -3,13 +3,12 @@ import com.typesafe.config.ConfigFactory;
 import com.varentech.deploya.daoimpl.EntriesDetailsDaoImpl;
 import com.varentech.deploya.form.Resource;
 import com.varentech.deploya.util.ConnectionConfiguration;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -17,112 +16,180 @@ import static org.junit.Assert.*;
 
 public class EntriesDetailsDaoImplTest {
 
-    @Test
-    public void insertIntoEntriesTest() throws SQLException {
-        Resource res = new Resource();
-        EntriesDetailsDaoImpl impl = new EntriesDetailsDaoImpl();
+    public EntriesDetailsDaoImpl impl = new EntriesDetailsDaoImpl();
+    public static Resource res = new Resource();
 
-        Date date = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"); //format the date to have no spaces
-        String formatted_time = formatter.format(date);
+    public  static  Config fileConf = ConfigFactory.parseFile(new File("application.conf"));
+    public static Config config = ConfigFactory.load(fileConf);
+    public static Connection connection = ConnectionConfiguration.getConnection();
 
-        Config fileConf = ConfigFactory.parseFile(new File("application.conf"));
-        Config config = ConfigFactory.load(fileConf);
 
+    @BeforeClass
+    public static void setUp() {
         ConnectionConfiguration.setPathToDataBase(config.getString("varentech.project.path_to_database"));
+    }
 
-        //add to entry object
-        res.entry.setPathToDestination("path");
-        res.entry.setExecuteArguments("execute");
-        res.entry.setUnpackArguments("unpack");
-        res.entry.setTime(formatted_time);
-        res.entry.setFileName("file");
-        res.entry.setUserName("katie");
-        res.entry.setArchive("archive");
-        res.entry.setPathToLocalFile("local");
-
-        impl.insertIntoEntries();
-
-        Connection connection = ConnectionConfiguration.getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(
-                "SELECT * FROM Entries WHERE time_stamp = " + "'" + res.entry.getTime() + "'"
-        );
-
-
-        assertNotEquals("",resultSet.getString(1));
-        assertEquals(formatted_time, resultSet.getString(2));
-        assertEquals("katie", resultSet.getString(3));
-        assertEquals("file", resultSet.getString(4));
-        assertEquals("local", resultSet.getString(5));
-        assertEquals("path", resultSet.getString(6));
-        assertEquals("unpack", resultSet.getString(7));
-        assertEquals("execute", resultSet.getString(8));
-        assertEquals("archive", resultSet.getString(9));
-
-        resultSet.close();
-        statement.close();
+    @AfterClass
+    public static void breakDown() throws SQLException {
         connection.close();
     }
 
     @Test
-    public void insertIntoEntriesDetailsTest() throws SQLException {
-        Resource res = new Resource();
-        EntriesDetailsDaoImpl impl = new EntriesDetailsDaoImpl();
+    public void createEntriesTableTest() throws SQLException {
+        DatabaseMetaData metaData = ConnectionConfiguration.getConnection().getMetaData();
+        //check to see if Entries table exists.
+        ResultSet resultSet = metaData.getTables(null, null, "Entries", null);
+        if (!resultSet.next()) {
+            resultSet.close();
+            impl.createEntriesTable();
 
-        Date date = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"); //format the date to have no spaces
-        String formatted_time = formatter.format(date);
+            metaData = ConnectionConfiguration.getConnection().getMetaData();
+            //check to see if Entries table exists.
+            resultSet = metaData.getTables(null, null, "Entries", null);
+            assertTrue(resultSet.next());
+            resultSet.close();
+        }
+    }
 
+    @Test
+    public void createEntriesDetailsTableTest() throws SQLException {
+        DatabaseMetaData metaData = ConnectionConfiguration.getConnection().getMetaData();
+        //check to see if Entries_Details table exists.
+        ResultSet resultSet = metaData.getTables(null, null, "Entries_Details", null);
+        if (!resultSet.next()) {
+            impl.createEntriesDetailsTable();
+            resultSet.close();
+
+            metaData = ConnectionConfiguration.getConnection().getMetaData();
+            //check to see if Entries Details table exists.
+            resultSet = metaData.getTables(null, null, "Entries_Details", null);
+            assertTrue(resultSet.next());
+            resultSet.close();
+        }
+    }
+
+   /* @Test
+    public void insertColumnsIntoEntriesTest() throws SQLException {
         Config fileConf = ConfigFactory.parseFile(new File("application.conf"));
         Config config = ConfigFactory.load(fileConf);
-
         ConnectionConfiguration.setPathToDataBase(config.getString("varentech.project.path_to_database"));
 
-        res.entry.setPathToDestination("path");
-        res.entry.setExecuteArguments("execute");
-        res.entry.setUnpackArguments("unpack");
-        res.entry.setTime(formatted_time);
-        res.entry.setFileName("file");
-        res.entry.setUserName("katie");
-        res.entry.setArchive("archive");
-        res.entry.setPathToLocalFile("local");
+        DatabaseMetaData metaData = ConnectionConfiguration.getConnection().getMetaData();
+        //check to see if Entries table exists.
+        ResultSet resultSet = metaData.getTables(null, null, "Entries", null);
+        if (resultSet.next()) {
+            impl.insertColumnsIntoEntries();
+        }
 
-        impl.insertIntoEntries();
+    }*/
 
-        res.entriesDetail.setError("error");
-        res.entriesDetail.setFileName("file");
-        res.entriesDetail.setHashValue("hash");
-        res.entriesDetail.setOutput("output");
-
-        impl.insertIntoEntriesDetail(res.entriesDetail);
-
+    @Test
+    public void insertIntoEntriesTest() throws SQLException {
+        Config fileConf = ConfigFactory.parseFile(new File("application.conf"));
+        Config config = ConfigFactory.load(fileConf);
         Connection connection = ConnectionConfiguration.getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(
-                "SELECT id FROM Entries WHERE time_stamp = " + "'" + res.entry.getTime() + "'"
-        );
-        int id = resultSet.getInt("id");
+        ConnectionConfiguration.setPathToDataBase(config.getString("varentech.project.path_to_database"));
 
-        resultSet.close();
-        statement.close();
-        connection.close();
 
-        connection = ConnectionConfiguration.getConnection();
-        statement = connection.createStatement();
-        resultSet = statement.executeQuery(
-                "SELECT * FROM Entries_Details WHERE entries_id = " + "'" + id + "'"
-        );
+        DatabaseMetaData metaData = ConnectionConfiguration.getConnection().getMetaData();
+        //check to see if Entries table exists.
+        ResultSet resultSet = metaData.getTables(null, null, "Entries", null);
+        if (resultSet.next()) {
+            resultSet.close();
+            Date date = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"); //format the date to have no spaces
+            String formatted_time = formatter.format(date);
 
-        assertNotEquals("",resultSet.getString(1));
-        assertEquals(String.valueOf(id), resultSet.getString(2));
-        assertEquals("file", resultSet.getString(3));
-        assertEquals("hash", resultSet.getString(4));
-        assertEquals("output", resultSet.getString(5));
-        assertEquals("error", resultSet.getString(6));
+            //add to entry object
+            res.entry.setPathToDestination("path");
+            res.entry.setExecuteArguments("execute");
+            res.entry.setUnpackArguments("unpack");
+            res.entry.setTime(formatted_time);
+            res.entry.setFileName("file");
+            res.entry.setUserName("katie");
+            res.entry.setArchive("archive");
+            res.entry.setPathToLocalFile("local");
 
-        resultSet.close();
-        statement.close();
-        connection.close();
+            impl.insertIntoEntries();
+
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery(
+                    "SELECT * FROM Entries WHERE time_stamp = " + "'" + res.entry.getTime() + "'"
+            );
+
+            assertNotEquals("", resultSet.getString(1));
+            assertEquals(formatted_time, resultSet.getString(2));
+            assertEquals("katie", resultSet.getString(3));
+            assertEquals("file", resultSet.getString(4));
+            assertEquals("local", resultSet.getString(5));
+            assertEquals("path", resultSet.getString(6));
+            assertEquals("unpack", resultSet.getString(7));
+            assertEquals("execute", resultSet.getString(8));
+            assertEquals("archive", resultSet.getString(9));
+
+            resultSet.close();
+            statement.close();
+        }
+    }
+
+    @Test
+    public void insertIntoEntriesDetailsTest() throws SQLException {
+        Config fileConf = ConfigFactory.parseFile(new File("application.conf"));
+        Config config = ConfigFactory.load(fileConf);
+        Connection connection = ConnectionConfiguration.getConnection();
+        ConnectionConfiguration.setPathToDataBase(config.getString("varentech.project.path_to_database"));
+
+        DatabaseMetaData metaData = ConnectionConfiguration.getConnection().getMetaData();
+        //check to see if Entries table exists.
+        ResultSet resultSet = metaData.getTables(null, null, "Entries_Details", null);
+        if (resultSet.next()) {
+            resultSet.close();
+
+            Date date = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"); //format the date to have no spaces
+            String formatted_time = formatter.format(date);
+
+            res.entry.setPathToDestination("path");
+            res.entry.setExecuteArguments("execute");
+            res.entry.setUnpackArguments("unpack");
+            res.entry.setTime(formatted_time);
+            res.entry.setFileName("file");
+            res.entry.setUserName("katie");
+            res.entry.setArchive("archive");
+            res.entry.setPathToLocalFile("local");
+
+            impl.insertIntoEntries();
+
+            res.entriesDetail.setError("error");
+            res.entriesDetail.setFileName("file");
+            res.entriesDetail.setHashValue("hash");
+            res.entriesDetail.setOutput("output");
+
+            impl.insertIntoEntriesDetail(res.entriesDetail);
+
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery(
+                    "SELECT id FROM Entries WHERE time_stamp = " + "'" + res.entry.getTime() + "'"
+            );
+            int id = resultSet.getInt("id");
+
+            resultSet.close();
+            statement.close();
+
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(
+                    "SELECT * FROM Entries_Details WHERE entries_id = " + "'" + id + "'"
+            );
+
+            assertNotEquals("", resultSet.getString(1));
+            assertEquals(String.valueOf(id), resultSet.getString(2));
+            assertEquals("file", resultSet.getString(3));
+            assertEquals("hash", resultSet.getString(4));
+            assertEquals("output", resultSet.getString(5));
+            assertEquals("error", resultSet.getString(6));
+
+            resultSet.close();
+            statement.close();
+        }
     }
 }
