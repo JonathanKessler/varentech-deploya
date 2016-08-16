1f00b748eb2aca1c57f2fe7b5220c5e83f2cfb94
kesslerk <kesslerk@varentech.com>
2016-08-16 13:37:40 -0400

    Add files via upload

--- src/test/java/FormServletTest.java
+++ src/test/java/FormServletTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -32,6 +33,25 @@ public class FormServletTest {
         String home = System.getProperty("user.home");
         String path = home + File.separator + "Documents";
         assertEquals(path, formServlet.homeDirectory("~" + File.separator + "Documents"));
+    }
+
+    @Test
+    public void createDirectoryDBTest(){
+        FormServlet formServlet = new FormServlet();
+
+        String path = "src/test/resources/Deploya/test.db";
+        assertTrue(formServlet.createDirectoryDB(path));
+
+        File file = new File("src/test/resources/Deploya");
+        assertTrue(file.isDirectory());
+
+        file.delete();
+
+        path = "src/test/nope/Deploya/test.db";
+        assertFalse(formServlet.createDirectoryDB(path));
+
+        file = new File("src/test/nope/Deploya");
+        assertFalse(file.exists());
     }
 
 }
